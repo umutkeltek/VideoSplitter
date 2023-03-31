45d491243f1c2e22dad28df9c996066783b10185
Umut Keltek <umut.keltek@gmail.com>
2023-03-31 05:25:05 +0300

    Add VideoSplitterWindow class
    Commit description: This commit adds the VideoSplitterWindow class, which defines the graphical user interface of the application using PyQt5 widgets. The initUI method initializes the UI elements and lays them out using a vertical box layout. The add_section, load_sections_from_file, browse_input_video, browse_output_folder, parse_sections_file, process_video, time_str_to_seconds, seconds_to_time_str, and split_video methods handle the functionality of the UI elements. The main function initializes the VideoSplitterWindow and runs the application.

--- video_splitter.py
+++ video_splitter.py
@@ -5,6 +5,7 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QGridLayout, QWidget, QPu
     QFileDialog, QHBoxLayout, QVBoxLayout, QTableWidget, QHeaderView, QProgressBar, QTableWidgetItem
 from moviepy.editor import VideoFileClip
 
+
 class VideoSplitterWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -32,18 +33,6 @@ class VideoSplitterWindow(QMainWindow):
 
         layout.addLayout(input_layout)
 
-        # Output folder
-        output_layout = QHBoxLayout()
-        self.output_label = QLabel('Output Folder:')
-        self.output_line_edit = QLineEdit()
-        self.output_button = QPushButton('Browse')
-        self.output_button.clicked.connect(self.browse_output_folder)
-        output_layout.addWidget(self.output_label)
-        output_layout.addWidget(self.output_line_edit)
-        output_layout.addWidget(self.output_button)
-
-        layout.addLayout(output_layout)
-
         # Sections table
         self.sections_table = QTableWidget()
         self.sections_table.setColumnCount(3)
@@ -58,6 +47,25 @@ class VideoSplitterWindow(QMainWindow):
         self.add_section_button.clicked.connect(self.add_section)
         layout.addWidget(self.add_section_button)
 
+        # Load sections from file button
+        self.load_sections_button = QPushButton('Load Sections From File')
+        self.load_sections_button.clicked.connect(self.load_sections_from_file)
+        layout.addWidget(self.load_sections_button)
+
+        layout.addLayout(QHBoxLayout())
+
+        # Output folder
+        output_layout = QHBoxLayout()
+        self.output_label = QLabel('Output Folder:')
+        self.output_line_edit = QLineEdit()
+        self.output_button = QPushButton('Browse')
+        self.output_button.clicked.connect(self.browse_output_folder)
+        output_layout.addWidget(self.output_label)
+        output_layout.addWidget(self.output_line_edit)
+        output_layout.addWidget(self.output_button)
+
+        layout.addLayout(output_layout)
+
         # Process video button
         self.process_button = QPushButton('Process Video')
         self.process_button.clicked.connect(self.process_video)
@@ -70,10 +78,28 @@ class VideoSplitterWindow(QMainWindow):
 
         widget.setLayout(layout)
 
-    # Add necessary functions for handling UI events
+    def add_section(self):
+        row = self.sections_table.rowCount()
+        self.sections_table.insertRow(row)
+        self.sections_table.setItem(row, 0, QTableWidgetItem(''))
+        self.sections_table.setItem(row, 1, QTableWidgetItem(''))
+        self.sections_table.setItem(row, 2, QTableWidgetItem(''))
+
+    def load_sections_from_file(self):
+        file_name, _ = QFileDialog.getOpenFileName(self, 'Open Sections File', '', 'Text Files (*.txt);;All Files (*)')
+        if file_name:
+            sections = self.parse_sections_file(file_name)
+            self.sections_table.setRowCount(0)
+            for section in sections:
+                row = self.sections_table.rowCount()
+                self.sections_table.insertRow(row)
+                self.sections_table.setItem(row, 0, QTableWidgetItem(section['title']))
+                self.sections_table.setItem(row, 1, QTableWidgetItem(self.seconds_to_time_str(section['start_time'])))
+                self.sections_table.setItem(row, 2, QTableWidgetItem(self.seconds_to_time_str(section['end_time'])))
 
     def browse_input_video(self):
-        file_name, _ = QFileDialog.getOpenFileName(self, 'Open Video File', '', 'Video Files (*.mp4 *.avi *.mkv *.mov);;All Files (*)')
+        file_name, _ = QFileDialog.getOpenFileName(self, 'Open Video File', '',
+                                                   'Video Files (*.mp4 *.avi *.mkv *.mov);;All Files (*)')
         if file_name:
             self.input_line_edit.setText(file_name)
 
@@ -82,12 +108,23 @@ class VideoSplitterWindow(QMainWindow):
         if folder_name:
             self.output_line_edit.setText(folder_name)
 
-    def add_section(self):
-        row = self.sections_table.rowCount()
-        self.sections_table.insertRow(row)
-        self.sections_table.setItem(row, 0, QTableWidgetItem(''))
-        self.sections_table.setItem(row, 1, QTableWidgetItem(''))
-        self.sections_table.setItem(row, 2, QTableWidgetItem(''))
+    def parse_sections_file(self, sections_file):
+        with open(sections_file, 'r') as file:
+            lines = file.readlines()
+
+        sections = []
+        for line in lines:
+            match = re.match(r'(\d+:\d{2}:\d{2})\s*-\s*(.+)\s*-\s*(\d+:\d{2}:\d{2})$', line.strip())
+            if match:
+                start_time, title, end_time = match.groups()
+                sections.append({
+                    'title': title.strip(),
+                    'start_time': self.time_str_to_seconds(start_time),
+                    'end_time': self.time_str_to_seconds(end_time)
+                })
+
+        print(sections)
+        return sections
 
     def process_video(self):
         input_video = self.input_line_edit.text()
@@ -97,7 +134,6 @@ class VideoSplitterWindow(QMainWindow):
             return
 
         sections = []
-
         for row in range(self.sections_table.rowCount()):
             title = self.sections_table.item(row, 0).text()
             start_time = self.sections_table.item(row, 1).text()
@@ -106,12 +142,14 @@ class VideoSplitterWindow(QMainWindow):
             if title and start_time and end_time:
                 sections.append({'title': title, 'start_time': self.time_str_to_seconds(start_time),
                                  'end_time': self.time_str_to_seconds(end_time)})
+
         self.progress_bar.setValue(0)
         total_sections = len(sections)
-        for index, section in enumerate(sections):
-            split_video(input_video, output_folder, [section])
-            self.progress_bar.setValue((index + 1) * 100 / total_sections)
 
+        split_video(input_video, output_folder, sections)
+
+        for index, section in enumerate(sections):
+            self.progress_bar.setValue((index + 1) * 100 / total_sections)
 
     def time_str_to_seconds(self, time_str):
         time_parts = list(map(int, time_str.split(':')))
@@ -120,10 +158,8 @@ class VideoSplitterWindow(QMainWindow):
             seconds = seconds * 60 + part
         return seconds
 
-
-
-
-    
+    def seconds_to_time_str(self, seconds):
+        return f"{seconds // 3600:02d}:{(seconds % 3600) // 60:02d}:{seconds % 60:02d}"
 
 
 def split_video(input_video, output_folder, sections):
@@ -137,7 +173,6 @@ def split_video(input_video, output_folder, sections):
 
     video.close()
 
-
 def main():
     app = QApplication(sys.argv)
     window = VideoSplitterWindow()
@@ -146,3 +181,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
